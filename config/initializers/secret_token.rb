0539ca6a73d0eda580e6d70415a4066aea6fe2b5
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-09-30 21:01:22 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListKungFu::Application.config.secret_token = 'e2d4bf66bb282b6079b439d7d181977ac8ff0791b3525eeba78bd74ce641115753ed9a9071d9e47dca6503c2c721e15aed849c757e20311ca585ed00e76a3115'
