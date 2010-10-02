2feb2ac5157b886d5491fd5ae275585e18759419
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-10-02 20:53:09 +0200

    adding factory girl for tests

--- Gemfile
+++ Gemfile
@@ -2,15 +2,17 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.0.rc'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'paperclip'
 gem 'devise', :git => "git://github.com/plataformatec/devise.git", :tag => "v1.1.rc2"
 gem 'rmagick' # for paperclip
 gem 'RedCloth' # for textile support
+gem 'aws-s3', :require => 'aws/s3'
 
+group :development, :test do
+	gem 'ruby-debug'
+  gem 'factory_girl_rails'
+end
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -18,18 +20,6 @@ gem 'RedCloth' # for textile support
 # Deploy with Capistrano
 # gem 'capistrano'
 
-# To use debugger
-# gem 'ruby-debug'
-
 # Bundle the extra gems:
 # gem 'bj'
 # gem 'nokogiri', '1.4.1'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'aws-s3', :require => 'aws/s3'
-
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
---
+++ test/factories.rb
@@ -0,0 +1,25 @@
+Factory.define :user do |f|
+  f.password "password"
+  f.sequence(:email) { |n| "user#{n}@listkungfuqa.com" }
+end
+
+Factory.define :list do |f|
+  f.sequence(:title) { |n| "List #{n}" }
+  f.association :owner, :factory => :user
+end
+
+Factory.define :list_item do |f|
+  f.body "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus elementum tempus. Fusce nibh elit, cursus at porttitor elementum, consectetur vel diam. Curabitur vehicula suscipit tincidunt. Proin ac purus diam, vitae accumsan mauris. Cras diam nunc, adipiscing sed dapibus pellentesque, ornare quis justo. Suspendisse nisi nulla, congue placerat cursus vitae, euismod dapibus tortor. Pellentesque tellus sapien, malesuada a pulvinar id, sagittis in orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras vehicula urna at mauris iaculis imperdiet. Donec iaculis, urna sed volutpat vestibulum, ipsum dolor sodales arcu, vel vestibulum velit quam et risus."
+  f.completed false
+  f.parent nil
+  f.association :list
+end
+
+Factory.define :tag do |f|
+  f.sequence(:name) { |n| "Tag #{n}" }
+end
+
+Factory.define :filter do |f|
+  f.sequence(:name) { |n| "Filter #{n}" }
+  f.searchtext ""
+end
