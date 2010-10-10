4ab31d25d971ab20ef1f4c913be90543a0304c9f
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-10-10 13:01:20 +0200

    moving aways fromj uki.js. to complicated to use trying UI.Layout (jquery plugin) now.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,32 +2,6 @@
 	Create main UI
 */
 
-var mainUI = uki(
-{
-	view: 'HSplitPane'
-	, rect: '1000 600'
-	, anchors: 'left top right bottom'
-	, handlePosition: 300
-	, leftMin: 300
-	, rightMin: 500
-	, leftChildViews: [
-			{ view: 'VSplitPane'
-				, rect: '300 600'
-				, anchors: 'left top right bottom'
-				, vertical: true
-				, handlePosition: 400
-				, topMin: 400
-				, bottomMin: 100
-				, topChildViews: [
-					{ view: 'ScrollPane'
-						, rect: '300 400'
-						, anchors: 'top left right bottom'
-						, id: 'ListViewPane'
-					}
-				]
-			}
-		]
-	,	rightChildViews: {  }
-}).attachTo( $("#application").get(0), '1000 600' );
 
-$("#ListViewPane").ListView();
+
+//$("#ListViewPane").ListView();
