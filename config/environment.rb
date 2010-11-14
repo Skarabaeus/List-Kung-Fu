3ba050c8eb12b9131999592f927cedd1885aa1da
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-11-14 13:05:27 +0100

    adding confirmation mail.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ListKungFu::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address        => 'smtp.gmail.com',
+  :port           => 587,
+  :domain         => 'listkungfu.com',
+  :authentication => :plain,
+  :user_name      => 'registration@listkungfu.com',
+  :password       => ENV['REGISTRATION_PASSWORD']
+}
--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,9 @@ ActiveRecord::Schema.define(:version => 20101107200137) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
