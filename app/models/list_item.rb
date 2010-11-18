6dae0cc4f72e40c5a24c5767f71d29a82d7a140a
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-11-18 22:04:46 +0100

    convert URL's automatically to links

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -2,7 +2,7 @@ class ListItem < ActiveRecord::Base
   # ASSOCIATIONS
 
   belongs_to :list
-  
+
   # for now not supporting this, because I'm not yet pursuated that
   # this feature is really needed. simple todo lists are more likely
   # to be actually used.
@@ -25,9 +25,32 @@ class ListItem < ActiveRecord::Base
     !self.id.nil?
   end
 
+  def stefan
+    0
+  end
+
+
   private
 
   def body_rendered
-    write_attribute( :body_rendered, RedCloth.new( self.body ).to_html ) unless self.body.nil?
+
+    unless self.body.nil?
+      # matches URLs
+      regex = /(\s|>)((https?|ftp):(\/\/)+([\w\d:\/\#@%;$()~_?\+-=\\\&][^<]*))(\s|<)/
+
+      # generate HTML from textile
+      html = RedCloth.new( self.body ).to_html
+
+      # match URLs
+      matches = html.scan(regex).collect{ |match| match.second }
+      matches.uniq!
+
+      matches.each do |match|
+        html.gsub!(match, %Q-<a href="#{match}" target="_blank">#{match}</a>-)
+      end
+
+
+      write_attribute( :body_rendered, html )
+    end
   end
 end
