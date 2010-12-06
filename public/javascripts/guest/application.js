18c0d465dd3fa7ea10d869ca7957e0d31ea0c4b7
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-12-06 20:42:44 +0100

    profile view fix for low screen resolutions.

--- public/javascripts/guest/application.js
+++ public/javascripts/guest/application.js
@@ -1,7 +1,13 @@
 $( document ).ready(function(){
 	var padding = $( "#wrapper" ).css( "paddingTop" );
 	padding = parseInt( padding.substring( 0, padding.length - 2 ) );
+
+	// adjust height.
+	var height = $( window ).height() - ( $( window ).height() / 100 * 30 );
+	$( "#wrapper" ).css( "height", height + "px" );
 	
-	var marginTop = ( $(window).height() / 2 ) - ( $( "#wrapper" ).height() / 2 ) - padding;
+	// adjust margin top
+	var marginTop = ( $( window ).height() / 2 ) - ( $( "#wrapper" ).height() / 2 ) - padding;
 	$( "#wrapper" ).css( "marginTop",  marginTop + "px" );
+
 });
