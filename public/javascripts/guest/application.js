39c4be0c195403aa045f13f32b488f7d792862ec
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2010-12-30 19:24:47 +0100

    didn't really like all the automatic padding, centering and height adjusting. therefore removed it and use static padding now combined with overflow scrolling

--- public/javascripts/guest/application.js
+++ public/javascripts/guest/application.js
@@ -1,13 +1,3 @@
 $( document ).ready(function(){
-	var padding = $( "#wrapper" ).css( "paddingTop" );
-	padding = parseInt( padding.substring( 0, padding.length - 2 ) );
-
-	// adjust height.
-	var height = $( window ).height() - ( $( window ).height() / 100 * 30 );
-	$( "#wrapper" ).css( "height", height + "px" );
-	
-	// adjust margin top
-	var marginTop = ( $( window ).height() / 2 ) - ( $( "#wrapper" ).height() / 2 ) - padding;
-	$( "#wrapper" ).css( "marginTop",  marginTop + "px" );
 
 });
