7196f48091ebc21006b94ec1489d93d6cbd52f91
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2011-01-16 23:41:45 +0100

    continue work on dashboard

--- public/javascripts/jquery.Dashboard.js
+++ public/javascripts/jquery.Dashboard.js
@@ -14,10 +14,24 @@
 			},
 
 			Show: function() {
+				widget.header = $( '<div id="dashboard-header" class="header">Dashboard</div>')
 				widget.wrapper = $( '<div class="ui-layout-content" id="dashboard-view"></div>' );
-				widget.element.append(widget.wrapper);
 
-				widget.wrapper.text( "Dashboard - to be implemented soon" );
+				widget.today = $( '<div id="today" class="schedule-column"><h1>Today</h1></div>' );
+				widget.tomorrow = $( '<div id="tomorrow" class="schedule-column"><h1>Tomorrow</h1></div>' );
+				widget.nextweek = $( '<div id="nextweek" class="schedule-column"><h1>Next Week</h1></div>' );
+				widget.later = $( '<div id="later" class="schedule-column"><h1>Later</h1></div>' );
+				widget.whenever = $( '<div id="whenever" class="schedule-column"><h1>Whenever</h1> \
+					</div><div style="clear:both;">&nbsp;</div>' );
+
+				widget.element.append( widget.header );
+				widget.element.append( widget.wrapper );
+
+				widget.wrapper.append( widget.today );
+				widget.wrapper.append( widget.tomorrow );
+				widget.wrapper.append( widget.nextweek );
+				widget.wrapper.append( widget.later );
+				widget.wrapper.append( widget.whenever );
 
 				_TriggerResize();
 			},
@@ -25,6 +39,7 @@
 			Hide: function() {
 				if ( widget.wrapper ) {
 					widget.wrapper.remove();
+					widget.header.remove();
 				}
 			},
 
