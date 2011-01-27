b3d295e4d608ab6c3ab9ae77e28194eead92b95d
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2011-01-27 23:07:25 +0100

    fixing default host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ ListKungFu::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'listkungfu.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'listkungfu.com' }
 end
