9faaf6bb35253e99f21317df149115fa59d6f4d3
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2011-01-27 22:57:31 +0100

    re-enabling gmail smtp

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ListKungFu::Application.initialize!
 
-#ActionMailer::Base.smtp_settings = {
-#  :enable_starttls_auto => true,
-#  :address        => 'smtp.gmail.com',
-#  :port           => 587,
-#  :domain         => 'listkungfu.com',
-#  :authentication => :plain,
-#  :user_name      => 'registration@listkungfu.com',
-#  :password       => ENV['REGISTRATION_PASSWORD']
-#}
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address        => 'smtp.gmail.com',
+  :port           => 587,
+  :domain         => 'listkungfu.com',
+  :authentication => :plain,
+  :user_name      => 'registration@listkungfu.com',
+  :password       => ENV['REGISTRATION_PASSWORD']
+}
