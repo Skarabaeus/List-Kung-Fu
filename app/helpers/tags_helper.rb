688181f8bf28e20abdbb670b1d5b0bf28f1f62f1
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2011-02-04 21:53:45 +0100

    calculate average color from all tags assigned to a list.

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,22 +1,27 @@
 module TagsHelper
 
   def write_tag_colors
+    css = StringIO.new
+    TagsHelper::tag_colors.each_value do |color|
+      css << color.to_css
+    end
+
+    css.string.html_safe
   end
 
   class TagColor
-
     attr_accessor :class_name, :color_name, :background_color, :foreground_color
 
     def initialize( args )
       @class_name = args[ :class_name ]
-      @color_name = ''
-      @background_color = ''
-      @foreground_color = ''
+      @color_name = args[ :color_name ]
+      @background_color = args[ :background_color ]
+      @foreground_color = args[ :foreground_color ]
     end
 
     def to_css
       css = StringIO.new
-      css << ".#{@class_name}{ /* #{@color_name} */"
+      css << "\n.#{@class_name}{"
       css << "color:##{@foreground_color};"
       css << "background-color:##{@background_color};"
       css << "border: 1px solid ##{@background_color};}"
@@ -25,31 +30,35 @@ module TagsHelper
     end
 
   end
-  
-  # http://www.colchis.com/clrtable.html
-  TAG_COLOR_1  = TagColor.new( { :class_name => 'c1', :color_name => 'steelblue', :backround_color => '4682B4', :foreground_color => '222' } )
-  TAG_COLOR_2  = TagColor.new( { :class_name => 'c2', :color_name => 'royalblue', :backround_color => '4169E1', :foreground_color => '222' } )
-  TAG_COLOR_3  = TagColor.new( { :class_name => 'c3', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_4  = TagColor.new( { :class_name => 'c4', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_5  = TagColor.new( { :class_name => 'c5', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_6  = TagColor.new( { :class_name => 'c6', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_7  = TagColor.new( { :class_name => 'c7', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_8  = TagColor.new( { :class_name => 'c8', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_9  = TagColor.new( { :class_name => 'c9', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_10 = TagColor.new( { :class_name => 'c10', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_11 = TagColor.new( { :class_name => 'c11', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_12 = TagColor.new( { :class_name => 'c12', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_13 = TagColor.new( { :class_name => 'c13', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_14 = TagColor.new( { :class_name => 'c14', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_15 = TagColor.new( { :class_name => 'c15', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_16 = TagColor.new( { :class_name => 'c16', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_17 = TagColor.new( { :class_name => 'c17', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_18 = TagColor.new( { :class_name => 'c18', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_19 = TagColor.new( { :class_name => 'c19', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_20 = TagColor.new( { :class_name => 'c20', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_21 = TagColor.new( { :class_name => 'c21', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_22 = TagColor.new( { :class_name => 'c22', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_23 = TagColor.new( { :class_name => 'c23', :color_name => '', :backround_color => '', :foreground_color => '' } )
-  TAG_COLOR_24 = TagColor.new( { :class_name => 'c24', :color_name => '', :backround_color => '', :foreground_color => '' } )
 
+  # http://www.colchis.com/clrtable.html
+  def self.tag_colors
+    color = {}
+    color[ :c1 ] = TagColor.new( { :class_name => 'c1', :color_name => 'steelblue', :background_color => '4682B4', :foreground_color => '222222' } )
+    color[ :c2 ] = TagColor.new( { :class_name => 'c2', :color_name => 'royalblue', :background_color => '4169E1', :foreground_color => '222222' } )
+    color[ :c3 ] = TagColor.new( { :class_name => 'c3', :color_name => 'navy', :background_color => '000080', :foreground_color => 'eeeeee' } )
+    color[ :c4 ] = TagColor.new( { :class_name => 'c4', :color_name => 'mediumblue', :background_color => '0000CD', :foreground_color => 'eeeeee' } )
+    color[ :c5 ] = TagColor.new( { :class_name => 'c5', :color_name => 'dodgerblue', :background_color => '1E90FF', :foreground_color => '222222' } )
+    color[ :c6 ] = TagColor.new( { :class_name => 'c6', :color_name => 'lightskyblue', :background_color => '87CEFA', :foreground_color => '222222' } )
+    color[ :c7 ] = TagColor.new( { :class_name => 'c7', :color_name => 'mediumseagreen', :background_color => '3CB371', :foreground_color => '222222' } )
+    color[ :c8 ] = TagColor.new( { :class_name => 'c8', :color_name => 'darkgreen', :background_color => '006400', :foreground_color => 'eeeeee' } )
+    color[ :c9 ] = TagColor.new( { :class_name => 'c9', :color_name => 'limegreen', :background_color => '32CD32', :foreground_color => '222222' } )
+    color[ :c10 ] = TagColor.new( { :class_name => 'c10', :color_name => 'lawngreen', :background_color => '7CFC00', :foreground_color => '222222' } )
+    color[ :c11 ] = TagColor.new( { :class_name => 'c11', :color_name => 'lightgreen', :background_color => '90EE90', :foreground_color => '222222' } )
+    color[ :c12 ] = TagColor.new( { :class_name => 'c12', :color_name => 'olivedrab', :background_color => '6B8E23', :foreground_color => '222222' } )
+    color[ :c13 ] = TagColor.new( { :class_name => 'c13', :color_name => 'darkgoldenrod', :background_color => 'B8860B', :foreground_color => '222222' } )
+    color[ :c14 ] = TagColor.new( { :class_name => 'c14', :color_name => 'gold', :background_color => 'FFD700', :foreground_color => '222222' } )
+    color[ :c15 ] = TagColor.new( { :class_name => 'c15', :color_name => 'blanchedalmond', :background_color => 'FFEBCD', :foreground_color => '222222' } )
+    color[ :c16 ] = TagColor.new( { :class_name => 'c16', :color_name => 'blurrywood', :background_color => 'DEB887', :foreground_color => '222222' } )
+    color[ :c17 ] = TagColor.new( { :class_name => 'c17', :color_name => 'saddlebrown', :background_color => 'DEB887', :foreground_color => '222222' } )
+    color[ :c18 ] = TagColor.new( { :class_name => 'c18', :color_name => 'maroon', :background_color => '800000', :foreground_color => 'eeeeee' } )
+    color[ :c19 ] = TagColor.new( { :class_name => 'c19', :color_name => 'indianred', :background_color => 'CD5C5C', :foreground_color => '222222' } )
+    color[ :c20 ] = TagColor.new( { :class_name => 'c20', :color_name => 'coral', :background_color => 'FF7F50', :foreground_color => '222222' } )
+    color[ :c21 ] = TagColor.new( { :class_name => 'c21', :color_name => 'darkorange', :background_color => 'FF8C00', :foreground_color => '222222' } )
+    color[ :c22 ] = TagColor.new( { :class_name => 'c22', :color_name => 'DC143C', :background_color => 'DC143C', :foreground_color => '222222' } )
+    color[ :c23 ] = TagColor.new( { :class_name => 'c23', :color_name => 'deeppink', :background_color => 'FF1493', :foreground_color => '222222' } )
+    color[ :c24 ] = TagColor.new( { :class_name => 'c24', :color_name => '', :background_color => 'darkmagenta', :foreground_color => 'eeeeee' } )
+
+    color
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -16,7 +16,25 @@ class List < ActiveRecord::Base
   attr_accessible :title
 
   def tag_helper_color
-    "f00"
+    red, green, blue = 0, 0, 0
+
+    self.tags.each do |tag|
+      red = red + TagsHelper::tag_colors[ tag.color_class.to_sym ].background_color[ 0..1 ].to_i( 16 )
+      green = green + TagsHelper::tag_colors[ tag.color_class.to_sym ].background_color[ 2..3 ].to_i( 16 )
+      blue = blue + TagsHelper::tag_colors[ tag.color_class.to_sym ].background_color[ 4..5 ].to_i( 16 )
+    end
+
+    if tags.length > 0
+      mul_factor = 1.3
+
+      red = ( red / ( tags.length * mul_factor ) ).to_i
+      green = green / tags.length
+      blue = ( blue / ( tags.length * mul_factor ) ).to_i
+    else
+      red, green, blue = 255, 255, 255
+    end
+
+    color = red.to_s(16) + green.to_s(16) + blue.to_s(16)
   end
 
   def destroy_tag!( tag )
