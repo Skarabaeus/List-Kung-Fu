011e5edab68e5841215fa3931e874985052849cf
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2011-04-11 22:37:39 +0200

    let's be less strict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,8 +52,9 @@ class ApplicationController < ActionController::Base
   end
 
   def set_cache_buster
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
+    #response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    #response.headers["Pragma"] = "no-cache"
+    response.headers["Cache-Control"] = "must-revalidate"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
